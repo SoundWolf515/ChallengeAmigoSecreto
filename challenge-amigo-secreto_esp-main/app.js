0942d6daf81d78fd60085a1263543e92187813b4
SoundWolf <fabricio.gutierrez2212@gmail.com>
2025-02-10 11:26:06 -0500

    Funcion de registro agregada

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,12 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    let amigoIngresado = document.getElementById('amigo').value;
+    if (amigoIngresado == ""){
+        alert('Por favor, inserte un nombre primero');
+    }else{
+        amigos.push(amigoIngresado);
+        alert('Nombre ingresado');
+        console.log(amigos);
+    }
+}
